3cc6efdfa3bcec62f0604b3eb72bc00819f3987c
Leonardo Omaña <leotangram@gmail.com>
2021-06-04 11:03:01 -0500

    First commit

---
+++ models/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const path = require('path')
+const cors = require('cors')
+const Sockets = require('./sockets')
+
+class Server {
+  constructor() {
+    this.app = express()
+    this.port = process.env.PORT
+    this.server = http.createServer(this.app)
+    this.io = socketio(this.server, {
+      /* configurations */
+    })
+  }
+  middlewares() {
+    this.app.use(express.static(path.resolve(__dirname, '../public')))
+    this.app.use(cors())
+  }
+  configureSockets() {
+    new Sockets(this.io)
+  }
+  execute() {
+    this.middlewares()
+    this.configureSockets()
+    this.server.listen(this.port, () => {
+      console.log(`Server listening on port: ${this.port}`)
+    })
+  }
+}
+
+module.exports = Server
