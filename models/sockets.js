df0e15f812ff2061f75b10f2467bd76ec615898d
Leonardo Omaña <leotangram@gmail.com>
2021-06-08 10:10:34 -0500

    Sockets events

--- models/sockets.js
+++ models/sockets.js
@@ -1,15 +1,16 @@
+const BandList = require('./band-list')
+
 class Sockets {
   constructor(io) {
     this.io = io
+    this.bandlist = new BandList()
     this.socketEvents()
   }
 
   socketEvents() {
-    this.io.on('connection', (socket) => {
-      socket.on('message-to-server', (data) => {
-        console.log(data)
-        this.io.emit('message-from-server', data)
-      })
+    this.io.on('connection', socket => {
+      console.log('Connected client')
+      socket.emit('current-bands', this.bandlist.getBands())
     })
   }
 }
